e3f21187f5bcaf655a7e64c527cfdcbc4daa0790
Alan Bedoya <alanjbedoya@gmail.com>
2021-03-10 20:57:15 -0800

    Adding the option to delete a listing.

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,7 @@ import {listings} from './listings';
 const app = express();
 const port = 9000;
 
-const one = 1;
-const two = 2;
-
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
+app.use(bodyParser.json());
 
 //listing
 app.get('/listings', (_req, res) => {
@@ -16,7 +13,17 @@ app.get('/listings', (_req, res) => {
 })
 
 //delete-listing
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
 
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  
+  return res.send('Failed to delete listing.');
+})
 
 app.listen(port);
 
