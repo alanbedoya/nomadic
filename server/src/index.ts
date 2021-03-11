dced504b3421ebd973b1025934f26c3c2581b6ec
Alan Bedoya <alanjbedoya@gmail.com>
2021-03-10 22:21:43 -0800

    Added graphql to the project.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import {listings} from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-//listing
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-})
-
-//delete-listing
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  
-  return res.send('Failed to delete listing.');
-})
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
