e1c1b6a0bf4c165d51a469d8313173f0e1adb896
Alan Bedoya <alanjbedoya@gmail.com>
2021-03-17 22:41:53 -0700

    Updating my Apollo server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
