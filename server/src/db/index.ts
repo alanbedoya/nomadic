18b582f104fa628561996f005f49d474d8d7afb3
Alan Bedoya <alanjbedoya@gmail.com>
2021-03-19 01:37:54 -0700

    Connected monogoDB to project.

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,3 +1,19 @@
 import { MongoClient } from 'mongodb';
 
-export const connectDatabase = () => {};
+const user = 'user_001';
+const userPassword = 'r2tNzOnFnMdmGB3g';
+const cluster = 'cluster0.kpvh5';
+
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+export const connectDatabase = async () => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db('main');
+
+  return {
+    listings: db.collection('test_listings'),
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,25 @@
-import express from 'express';
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './db';
 import { typeDefs, resolvers } from './graphql';
 
-const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+  server.applyMiddleware({ app, path: '/api' });
 
-app.listen(port);
+  app.listen(port);
 
-console.log(`[app]: http://localhost:${port}`);
+  console.log(`[app]: http://localhost:${port}`);
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+};
+
+mount(express());
