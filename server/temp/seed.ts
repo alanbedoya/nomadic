759ac273d675566ead529c75d32d3f90c4194261
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-03 23:42:33 -0700

    Working on the database.

--- server/temp/seed.ts
+++ server/temp/seed.ts
@@ -22,6 +22,30 @@ const seed = async () => {
         numOfBaths: 2,
         rating: 5,
       },
+      {
+        _id: new ObjectId(),
+        title: 'Luxurious home with private pool',
+        image:
+          'https://res.cloudinary.com/tiny-house/image/upload/v1560645376/mock/Los%20Angeles/los-angeles-listing-1_aikhx7.jpg',
+        address: '100 Hollywood Hills Dr, Los Angeles, California',
+        price: 15000,
+        numOfGuests: 2,
+        numOfBeds: 1,
+        numOfBaths: 1,
+        rating: 4,
+      },
+      {
+        _id: new ObjectId(),
+        title: 'Single bedroom located in the heart of downtown San Fransisco',
+        image:
+          'https://res.cloudinary.com/tiny-house/image/upload/v1560646219/mock/San%20Fransisco/san-fransisco-listing-1_qzntl4.jpg',
+        address: '200 Sunnyside Rd, San Fransisco, California',
+        price: 25000,
+        numOfGuests: 3,
+        numOfBeds: 2,
+        numOfBaths: 2,
+        rating: 3,
+      },
       // ...
     ];
 
