3835cd1e9be6e1af33502b218c0cb76c19e2e0a2
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-06 02:37:03 -0700

    Wokring on the client folder.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<div>You can do this!!!</div>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
