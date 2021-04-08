22b9410baaa46c86bd6d5f8d068bd7ef72380856
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-08 01:23:56 -0700

    Working on the frontend now.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,10 @@ import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { Listings } from './sections/Listings';
 
-render(<Listings />, document.getElementById('root'));
+render(
+  <Listings title='WanderOffice Listings' />,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
