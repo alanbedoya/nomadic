3363f8466648683c185a5e0fbb1f183bfdf5fd8e
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-22 02:17:31 -0700

    Restructuring the project.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
-import { Listings } from './sections/Listings';
+import { Home, Host, Listing, Listings, NotFound, User } from './sections';
 import './styles/index.css';
 
 const client = new ApolloClient({
@@ -10,9 +11,24 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/host' component={Host} />
+        <Route exact path='/listing/:id' component={Listing} />
+        <Route exact path='/listings/:location?' component={Listings} />
+        <Route exact path='/user/:id' component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
 render(
   <ApolloProvider client={client}>
-    <Listings title='WanderOffice Listings' />
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
