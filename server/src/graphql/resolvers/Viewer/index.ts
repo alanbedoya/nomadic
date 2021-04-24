ae5a28aff8df24da297607eda422fc5fda44e4d4
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-24 02:23:24 -0700

    Working on updating the GraphQL resolvers to allow a user to login and logout.

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,17 +1,38 @@
+import crypto from 'crypto';
 import { IResolvers } from 'apollo-server-express';
+import { Google } from '../../../lib/api';
+import { Viewer } from '../../../lib/types';
+import { LogInArgs } from '../Viewer/types';
 
 export const viewerResolvers: IResolvers = {
   Query: {
-    authUrl: () => {
-      return 'Query.authUrl';
+    authUrl: (): string => {
+      try {
+        return Google.authUrl;
+      } catch (error) {
+        throw new Error(`Failed to query Google Auth Url: ${error}`);
+      }
     },
   },
+
   Mutation: {
-    logIn: () => {
-      return 'Mutation.logIn';
+    logIn: async (_root: undefined, { input }: LogInArgs, { db }) => {
+      try {
+        const code = input ? input.code : null;
+        const token = crypto.randomBytes(16).toString('hex');
+        const viewer = code ? await logInViaGoogle(code, token, db) : undefined;
+      } catch (error) {}
     },
     logOut: () => {
       return 'Mutation.logOut';
+    },
+  },
+  Viewer: {
+    id: (viewer: Viewer): string | undefined => {
+      return viewer._id;
+    },
+    hasWallet: (viewer: Viewer): boolean | undefined => {
+      return viewer.walletId ? true : undefined;
     },
   },
 };
