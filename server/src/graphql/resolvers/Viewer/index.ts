bf1862c0378caf201e820643e725682ed90cc8ca
Alan Bedoya <alanjbedoya@gmail.com>
2021-04-26 00:07:08 -0700

    graphql resolvers now working with google oath.

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -4,6 +4,76 @@ import { Google } from '../../../lib/api';
 import { Viewer, Database, User } from '../../../lib/types';
 import { LogInArgs } from '../Viewer/types';
 
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database
+): Promise<User | undefined> => {
+  const { user } = await Google.logIn(code);
+
+  if (!user) {
+    throw new Error('Google login error');
+  }
+
+  const userNamesList = user.names && user.names.length ? user.names : null;
+  const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+  const userEmailsList =
+    user.emailAddresses && user.emailAddresses.length
+      ? user.emailAddresses
+      : null;
+
+  const userName = userNamesList ? userNamesList[0].displayName : null;
+
+  const userId =
+    userNamesList &&
+    userNamesList[0].metadata &&
+    userNamesList[0].metadata.source
+      ? userNamesList[0].metadata.source.id
+      : null;
+
+  const userAvatar =
+    userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+  const userEmail =
+    userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+  if (!userId || !userName || !userAvatar || !userEmail) {
+    throw new Error('Google login error');
+  }
+
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: userId },
+    {
+      $set: {
+        name: userName,
+        avatar: userAvatar,
+        contact: userEmail,
+        token,
+      },
+    },
+    { returnOriginal: false }
+  );
+
+  let viewer = updateRes.value;
+
+  if (!viewer) {
+    const insertResult = await db.users.insertOne({
+      _id: userId,
+      token,
+      name: userName,
+      avatar: userAvatar,
+      contact: userEmail,
+      income: 0,
+      bookings: [],
+      listings: [],
+    });
+
+    viewer = insertResult.ops[0];
+  }
+
+  return viewer;
+};
+
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: (): string => {
@@ -20,7 +90,7 @@ export const viewerResolvers: IResolvers = {
       _root: undefined,
       { input }: LogInArgs,
       { db }: { db: Database }
-    ) => {
+    ): Promise<Viewer> => {
       try {
         const code = input ? input.code : null;
         const token = crypto.randomBytes(16).toString('hex');
@@ -43,8 +113,12 @@ export const viewerResolvers: IResolvers = {
         throw new Error(`Failed to log in: ${error}`);
       }
     },
-    logOut: () => {
-      return 'Mutation.logOut';
+    logOut: (): Viewer => {
+      try {
+        return { didRequest: true };
+      } catch (error) {
+        throw new Error(`Failed to log out: ${error}`);
+      }
     },
   },
   Viewer: {
